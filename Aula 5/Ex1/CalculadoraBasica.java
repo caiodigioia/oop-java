a57034fc52450e24e9e9e06d44e262c5279c2d09
Caio Di Gioia <81108520+caiodigioia@users.noreply.github.com>
2023-11-09 11:14:29 -0300

    Create CalculadoraBasica.java

---
+++ Aula 5/Ex1/CalculadoraBasica.java
@@ -0,0 +1,17 @@
+public class CalculadoraBasica implements MathOperation {
+
+    // Atributos
+    public int add (int a, int b) {
+        return a + b;
+    } 
+
+    public int sub (int a, int b) {
+        return a - b;
+    }
+
+    public int times (int a, int b) {
+        return a * b;
+    }
+
+    
+}
