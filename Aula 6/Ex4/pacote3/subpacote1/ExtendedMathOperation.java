9b5d7c826beb9743d256fe6db4d162c31e2d2893
Caio Di Gioia <81108520+caiodigioia@users.noreply.github.com>
2023-11-27 16:23:00 -0300

    Create ExtendedMathOperation.java

---
+++ Aula 6/Ex4/pacote3/subpacote1/ExtendedMathOperation.java
@@ -0,0 +1,10 @@
+package pacote3.subpacote1;
+
+import pacote1.MathOperation;
+
+public interface ExtendedMathOperation extends MathOperation {
+
+    public int div(int a, int b);
+    public int remainder(int a, int b);
+
+}
