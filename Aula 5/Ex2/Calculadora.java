3509970349782ca5d1cc24413125e0abc971cb23
Caio Di Gioia <81108520+caiodigioia@users.noreply.github.com>
2023-11-27 16:08:29 -0300

    Add files via upload

---
+++ Aula 5/Ex2/Calculadora.java
@@ -0,0 +1,13 @@
+public class Calculadora extends CalculadoraBasica {
+
+    // Métodos
+
+    public int div (int a, int b) {
+        return a / b;
+    }
+
+    public int remainder (int a, int b) {
+        return a % b;
+    } 
+
+}
