26117d546936f643dfbf39389539d86eb423b46e
Caio Di Gioia <81108520+caiodigioia@users.noreply.github.com>
2023-11-27 16:22:00 -0300

    Create Calculadora.java

---
+++ Aula 6/Ex4/pacote3/subpacote1/Calculadora.java
@@ -0,0 +1,17 @@
+package pacote3.subpacote1;
+
+import pacote1.*;
+
+public class Calculadora extends CalculadoraBasica {
+
+    // Métodos
+
+    public int div (int a, int b) {
+        return a / b;
+    }
+
+    public int remainder (int a, int b) {
+        return a % b;
+    } 
+
+}
